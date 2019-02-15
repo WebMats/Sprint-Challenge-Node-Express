59ed5bc3a66d33e669b5e41055c1911f1e101874
WebMats <lorlando127@gmail.com>
2019-02-15 11:47:51 -0500

    built the scaffold for the routes

---
+++ routes/action.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const actionDB = require('../data/helpers/actionModel');
+
+
+router.get('', async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        console.log(err)
+        throw err;
+    }
+})
+router.get('', async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        console.log(err)
+        throw err;
+    }
+})
+router.post('', async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        console.log(err)
+        throw err;
+    }
+})
+router.delete('', async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        console.log(err)
+        throw err;
+    }
+})
+router.update('', async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        console.log(err)
+        throw err;
+    }
+})
+
+module.exports = router;
